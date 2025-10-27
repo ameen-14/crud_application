f5ba1044734546cb990582f8a63bb38e67a03be4
Ameen <Ameen@NRH2F0690D.NRRSPL.NET>
2025-10-27 15:56:09 +0530

    Data edit and updated to sql and delete data

--- sqlapp/urls.py
+++ sqlapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 urlpatterns = [
     path("", views.insertpageview, name='insertpage'),
     path("insert/", views.InsertData, name="insert"),
-    path("show/", views.FetchData, name='show_data')
+    path("show/", views.FetchData, name='show_data'),
+    path("edit/<int:pk>", views.EditData, name='edit'),
+    path("update/<int:pk>", views.UpdateData, name="update"),
+    path("delete/<int:pk>", views.DeleteData, name="delete"),
+
 
 ]
--- sqlapp/views.py
+++ sqlapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 # Create your views here.
 def insertpageview(request):
     return render(request, "sqlapp/insert.html")
 
-def InsertData(request): #Data comes from html to view
+def InsertData(request): #Data comes from insert.html to view
     fname=request.POST["fname"]
     lname = request.POST["lname"]
     email= request.POST["email"]
@@ -13,9 +13,29 @@ def InsertData(request): #Data comes from html to view
     # creating object of Model class -- Inserting data into table
     newuser= Student.objects.create(firstname=fname, lastname=lname, email=email, contact=contact)
 
-    #after inserting data show on show.html
-    return render(request,"sqlapp/show.html")
+    #after inserting data, show on show.html
+    #return render(request,"sqlapp/show.html")
+    return redirect ('show_data')
 
 def FetchData(request):
     data = Student.objects.all()
     return render (request, "sqlapp/show.html", {'students': data })
+
+def EditData(request,pk): #Fetching the data on ID
+    get_data = Student.objects.get(id=pk)
+    print('PK :', pk)
+    return render (request, "sqlapp/edit.html", {'key1': get_data  })
+
+def UpdateData(request,pk):
+    udata= Student.objects.get(id=pk)
+    udata.firstname = request.POST['fname']
+    udata.lastname = request.POST['lname']
+    udata.email=request.POST['email']
+    udata.contact = request.POST['contact']
+    udata.save()
+    return redirect ('show_data')
+
+def DeleteData(request, pk):
+    ddata=Student.objects.get(id=pk)
+    ddata.delete()
+    return redirect('show_data')
